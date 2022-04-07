5893c55ec1307347a62c4258dd3e3d264ad6591d
Jack-Is-2048 <101245336+Jack-Is-2048@users.noreply.github.com>
2022-04-07 20:08:55 +1000

    Initial Project Commit

---
+++ hellopyside.py
@@ -0,0 +1,21 @@
+import sys
+from PySide6 import QtCore, QtWidgets
+
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.text = QtWidgets.QLabel("Hello World",
+                                     alignment=QtCore.Qt.AlignCenter)
+
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.text)
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+
+    widget = MyWidget()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec())
