a63c8b783faf8e1c12fee429e504245c3c517b22
Jack-Is-2048 <101245336+Jack-Is-2048@users.noreply.github.com>
2022-05-12 18:30:06 +1000

    Refactor for Pylint

--- tests/test_plot_widget.py
+++ tests/test_plot_widget.py
@@ -1,17 +1,31 @@
+"""
+Tests for plot_widget.py
+"""
 import pytest
 from plot_widget import PlotWidget
 
 
 class TestPlotWidget:
+    """
+    Encapsulates the test suite for plot_widget.py
+    """
     @pytest.fixture
-    def test_app(self, qtbot):
-        """sets up widget for testing"""
+    def test_app(self):
+        """
+        Sets up widget for testing
+        """
         test_app = PlotWidget()
 
         return test_app
 
     def test_plot_dcm(self, test_app):
+        """
+        Tests plot_widget test_plot_dcm function
+        """
         assert test_app.plot_dcm("test files/DICOM-RT-01/CT_3_Hashed.dcm")
 
     def test_clear_view(self, test_app):
+        """
+        Tests plot_widget test_clear_view function
+        """
         assert test_app.clear_view()
