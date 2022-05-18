3082223ca0a6ebdf8609fd1730fce9e3d6a4d381
Jack-Is-2048 <101245336+Jack-Is-2048@users.noreply.github.com>
2022-05-18 14:32:25 +1000

    Fix how dcm files are opened added testing

--- tests/test_plot_widget.py
+++ tests/test_plot_widget.py
@@ -1,6 +1,7 @@
 """
 Tests for plot_widget.py
 """
+import glob
 import pytest
 from plot_widget import PlotWidget
 
@@ -12,19 +13,34 @@ def test_app(qtbot):
     """
     test_app = PlotWidget()
     qtbot.addWidget(test_app)
-
     return test_app
+
+
+def test_update_plot(test_app):
+    """
+    Tests plot_widget update_plot function
+    """
+    assert test_app.set_paths(glob.glob(r"tests/test files/DICOM-RT-01/*.dcm"))
+    assert test_app.update_plot(2)
+
+
+def test_set_paths(test_app):
+    """
+    Tests plot_widget set_paths function
+    """
+    assert test_app.set_paths(glob.glob(r"tests/test files/DICOM-RT-01/*.dcm"))
 
 
 def test_plot_dcm(test_app):
     """
-    Tests plot_widget test_plot_dcm function
+    Tests plot_widget plot_dcm function
     """
-    assert test_app.plot_dcm("test files/DICOM-RT-01/CT_3_Hashed.dcm")
+    assert test_app.set_paths(glob.glob(r"tests/test files/DICOM-RT-01/*.dcm"))
+    assert test_app.plot_dcm(2)
 
 
 def test_clear_view(test_app):
     """
-    Tests plot_widget test_clear_view function
+    Tests plot_widget clear_view function
     """
     assert test_app.clear_view()
